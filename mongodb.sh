20fb72a223dfdd8377845643b9337a577b4c8027
kiran <kiranbabubachu@gmail.com>
2023-04-14 21:31:07 -0400

    create frontend application script

---
+++ mongodb.sh
@@ -0,0 +1,8 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+# Edit  the  file to replace 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
